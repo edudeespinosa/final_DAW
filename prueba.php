6ae8987f6363885c64d4b9a1944171156febe21c
edudeespinosa <edude.espinosa@gmail.com>
2014-05-02 14:12:11 -0500

    Mail done

--- prueba.php
+++ prueba.php
@@ -1,21 +1,43 @@
 <?php 
 
-//$sender = $_POST['sender'];
+session_start();
 $receiver = $_POST['receiver'];
 $subject = $_POST['subject'];
 $message = $_POST['message'];
-$header = 'From: LibrosChingones@elmejoequipo.com' . "\r\n" .
-    	  'Reply-To: LibrosChingones@elmejoequipo.com' . "\r\n" .
+$redirect = 0;
+$header = 'From: LibrosChingones@elmejorequipo.com' . "\r\n" .
+    	  'Reply-To: LibrosChingones@elmejorequipo.com' . "\r\n" .
     	  'X-Mailer: PHP/' . phpversion();
 
 $message = str_replace("\n.", "\n..", $message);
 
-$envio = mail($receiver, $subject, $message, $header);
+include_once("infousuario.php");
+if($res != false){
+
+	$messagefav = '<br> Hola '.$receiver.' Estos son mis libros favoritos... Cuales son los tuyos?<br><ul>';
+
+	foreach ($res as $key => $value) {
+		$messagefav.= '<li>'.$value['ISBN'].', '.$value['titulo'].', '.$value['autor'].', '.$value['editorial'].'</li>';
+
+	}
+	$messagefav.="</ul>";
+	$mensaje = $message . $messagefav;	
+	$envio = mail($receiver, $subject, $mensaje, $header);
+}
+
+
 
 if($envio == TRUE){
-	echo "Se envio con exito";
+	echo "<script>alert('Se envio con exito')</script>";
+	$redirect = 1;
+
 } else {
 	echo "Ocurrio un error";
+}
+
+if($redirect==1)
+{
+	header("Location: cuenta.php");
 }
 
 
